7a4680e3a4fddf490e6d9b397d94df7da42966d5
matthew633jdi <matthew633jdi@gmail.com>
2024-05-27 23:11:20 +0900

    init Plan of PBS project

---
+++ planofpbs/src/test/java/elgrage/matthew/planofpbs/PlanOfPbsApplicationTests.java
@@ -0,0 +1,13 @@
+package elgrage.matthew.planofpbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanOfPbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/elgrage/matthew/planofpbs/PlanOfPbsApplication.java
@@ -0,0 +1,13 @@
+package elgrage.matthew.planofpbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanOfPbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanOfPbsApplication.class, args);
+	}
+
+}
